c64d4e7f88992aecb265327a733454f259f5b1af
thiago-igor <thiago.maia0606@gmail.com>
2023-10-09 09:01:25 -0300

    Atualização

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Hero } from '../heroe.model';
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1 ,
     name: 'Wolverine'
   };
-  constructor() { }
 
-  ngOnInit(): void {
-      
-  }
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+ onSelect(hero: Hero): void{
+  this.selectedHero = hero;
+ }
+
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from "./heroe.model";
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Wolverine'},
+    {id: 2, name: 'Homem Aranha'},
+    {id: 3, name: 'Pantera Negra'},
+    {id: 4, name: 'Capitão America'},
+];
